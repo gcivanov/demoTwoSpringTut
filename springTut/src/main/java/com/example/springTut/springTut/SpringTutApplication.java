5e8bcc9c498064eddb61527901f6642ccdfb00af
GIvanov <givanov@GIvanovs-Mac-mini.local>
2017-07-15 19:39:58 +0300

    start project

---
+++ springTut/src/main/java/com/example/springTut/springTut/SpringTutApplication.java
@@ -0,0 +1,12 @@
+package com.example.springTut.springTut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTutApplication.class, args);
+	}
+}
